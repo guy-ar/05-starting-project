9f9897784a1a29d87cb86a7eba746a27cf3d0332
Guy Arbilly <Guy.Arbilly@gmail.com>
2025-02-27 23:45:15 +0200

    add the host - click event

--- src/app/safe-link.directive.ts
+++ src/app/safe-link.directive.ts
@@ -2,10 +2,22 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: 'a[appSafeLink]', // use as attribute and apply it to Anchor tag
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)' // host is used to add attributes or events to the host that holds the directive
+  }
 })
 export class SafeLinkDirective {
     constructor() {
         console.log('SafeLinkDirective constructor called');
+    }
+    onConfirmLeavePage(event: MouseEvent) {
+        console.log('SafeLinkDirective onConfirmLeavePage called');
+        const isWantToLeave = confirm('Are you sure you want to leave?');
+        if (isWantToLeave) {
+          return
+        } else {
+          event.preventDefault(); // prevent the default action of the event
+        }
     }
 
 }
